2f44da0d5c193080af506ce3ca14aa3867af0121
Michael Sanchez <michael.sanchez@uber.com>
2018-10-23 14:24:25 -0700

    Adding Empty Android App

---
+++ FlickrApplication/app/src/main/java/com/example/michaelsanchez/flickrapplication/FlickrApplication.java
@@ -0,0 +1,13 @@
+package com.example.michaelsanchez.flickrapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlickrApplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flickr_application);
+    }
+}
